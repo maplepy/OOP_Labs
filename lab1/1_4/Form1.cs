961b38546f7515b7711dd15d67f052d1ce81f96a
maplepy <sergienkoandrew9922@gmail.com>
2021-03-16 02:12:45 +0200

    lab1

---
+++ lab1/1_4/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_4
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Label label = new Label();
+            label.Text = "label " + count.ToString();
+            label.Font = new Font("Arial", 10, FontStyle.Bold | FontStyle.Italic);
+            label.Location = new Point(10, 40+20 * count++);
+            label.Click += new EventHandler(MyClick);
+            this.Controls.Add(label);
+        }
+        private void MyClick(object sender, EventArgs e)
+        {
+            label0.Text = "Останній натиснутий елемент під номером: " + sender.ToString().Split(" ")[3];
+        }
+    }
+}
---
+++ lab1/1_5/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_5
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Початок роботи";
+            textBox1.Text = "";
+            this.BackColor = Color.FromArgb(rand.Next(255), rand.Next(255), rand.Next(255));
+        }
+    }
+}
